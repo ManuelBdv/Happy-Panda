ceab012c72fefd426015736b57ca43ecf7b95e9d
ManuelBdv <manuelbcdev@gmail.com>
2022-11-27 18:58:02 -0600

    Generated component routes and fixed issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { CompraComponent } from './compra/compra.component';
 import { HomeComponent } from './home/home.component';
+import { VentaComponent } from './venta/venta.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'venta', component: VentaComponent},
+  {path: 'compra', component: CompraComponent},
+  {path: 'catalogo', component: CatalogoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { VentaComponent } from './venta/venta.component';
+import { CompraComponent } from './compra/compra.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    VentaComponent,
+    CompraComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule,
